857d861bb84930340a53e11ec18fadf2b90f7d7d
akffp88 <40857649+akffp88@users.noreply.github.com>
2018-08-20 16:02:32 +0900

    1

---
+++ 02.Animal_Classification_Model/test_model.py
@@ -0,0 +1,32 @@
+import tensorflow as tf
+
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+
+from animal_classification_model import animal_classifier
+
+
+zoo = pd.read_csv("./data/zoo.csv")
+
+animal_data = zoo.iloc[:,:-1]
+
+label_data = zoo.iloc[:,-1:]
+
+train_x, test_x, train_y, test_y = train_test_split(animal_data,label_data,test_size=0.3,random_state=42,stratify=label_data)
+
+train_name = train_x['animal_name']
+test_name = test_x['animal_name']
+
+train_x = train_x.iloc[:,1:]
+test_x = test_x.iloc[:,1:]
+
+model = animal_classifier(sess) 
+
+saver = tf.train.Saver()
+
+saver.restore(sess,tf.train.latest_checkpoint('./models/'))
+
+prediction_result = model._prediction_(test_x,test_y)
+
+print(prediction_result)
